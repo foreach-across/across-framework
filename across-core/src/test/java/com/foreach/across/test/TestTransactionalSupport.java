da1892f170067f37b58ccfcfd83bcf7f6062221a
Arne Vandamme <arne@foreach.be>
2014-03-01 11:08:31 +0100

    Put code style on project

--- across-core/src/test/java/com/foreach/across/test/TestTransactionalSupport.java
+++ across-core/src/test/java/com/foreach/across/test/TestTransactionalSupport.java
@@ -37,155 +37,158 @@ import static org.junit.Assert.*;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestTransactionalSupport.Config.class)
 @DirtiesContext
-public class TestTransactionalSupport {
-    @Autowired
-    private SessionFactory sessionFactory;
+public class TestTransactionalSupport
+{
+	@Autowired
+	private SessionFactory sessionFactory;
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Before
-    public void openSession() {
-        Session session = sessionFactory.openSession();
-        TransactionSynchronizationManager.bindResource(sessionFactory, new SessionHolder(session));
-    }
+	@Before
+	public void openSession() {
+		Session session = sessionFactory.openSession();
+		TransactionSynchronizationManager.bindResource( sessionFactory, new SessionHolder( session ) );
+	}
 
-    @After
-    public void closeSession() {
-        SessionFactoryUtils.closeSession(sessionFactory.getCurrentSession());
-        TransactionSynchronizationManager.unbindResource(sessionFactory);
-    }
+	@After
+	public void closeSession() {
+		SessionFactoryUtils.closeSession( sessionFactory.getCurrentSession() );
+		TransactionSynchronizationManager.unbindResource( sessionFactory );
+	}
 
-    @Test
-    public void singleModuleTransactional() {
-        assertNull(productRepository.getProductWithId(1));
+	@Test
+	public void singleModuleTransactional() {
+		assertNull( productRepository.getProductWithId( 1 ) );
 
-        Product product = new Product(1, "product 1");
-        productRepository.save(product);
+		Product product = new Product( 1, "product 1" );
+		productRepository.save( product );
 
-        closeSession();
-        openSession();
+		closeSession();
+		openSession();
 
-        Product other = productRepository.getProductWithId(1);
-        assertNotNull(other);
-        assertEquals(product, other);
-    }
+		Product other = productRepository.getProductWithId( 1 );
+		assertNotNull( other );
+		assertEquals( product, other );
+	}
 
-    @Test
-    public void otherModuleTransactional() {
-        assertNull(userRepository.getUserWithId(1));
+	@Test
+	public void otherModuleTransactional() {
+		assertNull( userRepository.getUserWithId( 1 ) );
 
-        User user = new User(1, "user 1");
-        userRepository.save(user);
+		User user = new User( 1, "user 1" );
+		userRepository.save( user );
 
-        closeSession();
-        openSession();
+		closeSession();
+		openSession();
 
-        User other = userRepository.getUserWithId(1);
-        assertNotNull(other);
-        assertEquals(user, other);
-    }
+		User other = userRepository.getUserWithId( 1 );
+		assertNotNull( other );
+		assertEquals( user, other );
+	}
 
-    @Test
-    public void combinedSave() {
-        Product product = new Product(2, "product 2");
-        User user = new User(2, "user 2");
+	@Test
+	public void combinedSave() {
+		Product product = new Product( 2, "product 2" );
+		User user = new User( 2, "user 2" );
 
-        userRepository.save(user, product);
+		userRepository.save( user, product );
 
-        closeSession();
-        openSession();
+		closeSession();
+		openSession();
 
-        User otherUser = userRepository.getUserWithId(2);
-        assertNotNull(otherUser);
-        assertEquals(user, otherUser);
+		User otherUser = userRepository.getUserWithId( 2 );
+		assertNotNull( otherUser );
+		assertEquals( user, otherUser );
 
-        Product otherProduct = productRepository.getProductWithId(2);
-        assertNotNull(otherProduct);
-        assertEquals(product, otherProduct);
-    }
+		Product otherProduct = productRepository.getProductWithId( 2 );
+		assertNotNull( otherProduct );
+		assertEquals( product, otherProduct );
+	}
 
-    @Test
-    public void combinedRollback() {
-        Product product = new Product(3, "product 3");
+	@Test
+	public void combinedRollback() {
+		Product product = new Product( 3, "product 3" );
 
-        boolean failed = false;
+		boolean failed = false;
 
-        try {
-            userRepository.save(null, product);
-        } catch (Exception e) {
-            failed = true;
-        }
+		try {
+			userRepository.save( null, product );
+		}
+		catch ( Exception e ) {
+			failed = true;
+		}
 
-        assertTrue(failed);
+		assertTrue( failed );
 
-        closeSession();
-        openSession();
+		closeSession();
+		openSession();
 
-        Product otherProduct = productRepository.getProductWithId(3);
-        assertNull(otherProduct);
-    }
+		Product otherProduct = productRepository.getProductWithId( 3 );
+		assertNull( otherProduct );
+	}
 
-    @Configuration
-    static class Config {
-        @Bean
-        public DataSource dataSource() throws Exception {
-            BasicDataSource dataSource = new BasicDataSource();
-            dataSource.setDriverClassName("org.hsqldb.jdbc.JDBCDriver");
-            dataSource.setUrl("jdbc:hsqldb:mem:acrosscore");
-            dataSource.setUsername("sa");
-            dataSource.setPassword("");
+	@Configuration
+	static class Config
+	{
+		@Bean
+		public DataSource dataSource() throws Exception {
+			BasicDataSource dataSource = new BasicDataSource();
+			dataSource.setDriverClassName( "org.hsqldb.jdbc.JDBCDriver" );
+			dataSource.setUrl( "jdbc:hsqldb:mem:acrosscore" );
+			dataSource.setUsername( "sa" );
+			dataSource.setPassword( "" );
 
-            return dataSource;
-        }
+			return dataSource;
+		}
 
-        @Bean
-        public LocalSessionFactoryBean sessionFactory() throws Exception {
-            LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-            sessionFactory.setDataSource(dataSource());
-            sessionFactory.setPackagesToScan("com.foreach.across.test.modules.hibernate1",
-                    "com.foreach.across.test.modules.hibernate2");
+		@Bean
+		public LocalSessionFactoryBean sessionFactory() throws Exception {
+			LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+			sessionFactory.setDataSource( dataSource() );
+			sessionFactory.setPackagesToScan( "com.foreach.across.test.modules.hibernate1",
+			                                  "com.foreach.across.test.modules.hibernate2" );
 
-            Properties p = new Properties();
-            p.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+			Properties p = new Properties();
+			p.setProperty( "hibernate.hbm2ddl.auto", "create-drop" );
 
-            sessionFactory.setHibernateProperties(p);
+			sessionFactory.setHibernateProperties( p );
 
-            return sessionFactory;
-        }
+			return sessionFactory;
+		}
 
-        @Bean
-        public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-            return new HibernateTransactionManager(sessionFactory);
-        }
+		@Bean
+		public HibernateTransactionManager transactionManager( SessionFactory sessionFactory ) {
+			return new HibernateTransactionManager( sessionFactory );
+		}
 
-        @Bean
-        public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-            return new PersistenceExceptionTranslationPostProcessor();
-        }
+		@Bean
+		public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+			return new PersistenceExceptionTranslationPostProcessor();
+		}
 
-        @Bean
-        public AcrossContext acrossContext(ConfigurableApplicationContext applicationContext) throws Exception {
-            AcrossContext acrossContext = new AcrossContext(applicationContext);
-            acrossContext.setDataSource(dataSource());
-            acrossContext.addModule(hibernate1Module());
-            acrossContext.addModule(hibernate2Module());
-            acrossContext.addApplicationContextConfigurer(new TransactionManagementConfigurer(), true);
+		@Bean
+		public AcrossContext acrossContext( ConfigurableApplicationContext applicationContext ) throws Exception {
+			AcrossContext acrossContext = new AcrossContext( applicationContext );
+			acrossContext.setDataSource( dataSource() );
+			acrossContext.addModule( hibernate1Module() );
+			acrossContext.addModule( hibernate2Module() );
+			acrossContext.addApplicationContextConfigurer( new TransactionManagementConfigurer(), true );
 
-            return acrossContext;
-        }
+			return acrossContext;
+		}
 
-        @Bean
-        public Hibernate1Module hibernate1Module() {
-            return new Hibernate1Module();
-        }
+		@Bean
+		public Hibernate1Module hibernate1Module() {
+			return new Hibernate1Module();
+		}
 
-        @Bean
-        public Hibernate2Module hibernate2Module() {
-            return new Hibernate2Module();
-        }
-    }
+		@Bean
+		public Hibernate2Module hibernate2Module() {
+			return new Hibernate2Module();
+		}
+	}
 }
