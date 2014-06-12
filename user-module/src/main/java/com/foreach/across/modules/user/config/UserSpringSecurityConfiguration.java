0db1adb05122db58fef7597f4fd2c24aaa000316
Arne Vandamme <arne@foreach.be>
2014-06-12 13:37:30 +0200

    CurrentUserProxy instead of CurrentPrincipal

--- user-module/src/main/java/com/foreach/across/modules/user/config/UserSpringSecurityConfiguration.java
+++ user-module/src/main/java/com/foreach/across/modules/user/config/UserSpringSecurityConfiguration.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
@@ -31,7 +30,7 @@ public class UserSpringSecurityConfiguration extends WebSecurityModuleConfigurer
 	}
 
 	@Bean
-	public CurrentUserProxy currentPrincipal() {
+	public CurrentUserProxy currentUserProxy() {
 		return new CurrentUserProxyImpl();
 	}
 
