ca6649d9e7b5562603d83e239292c8cd6d1c4424
Arne Vandamme <arne@foreach.be>
2014-07-30 15:09:15 +0200

    Lowercase across_sequences table

--- across-core/src/main/java/com/foreach/across/core/database/AcrossSchemaConfiguration.java
+++ across-core/src/main/java/com/foreach/across/core/database/AcrossSchemaConfiguration.java
@@ -2,7 +2,7 @@ package com.foreach.across.core.database;
 
 public final class AcrossSchemaConfiguration
 {
-	public static final String TABLE_SEQUENCES = "ACROSS_SEQUENCES";
+	public static final String TABLE_SEQUENCES = "across_sequences";
 	public static final String SEQUENCE_NAME = "seq_name";
 	public static final String SEQUENCE_VALUE = "seq_number";
 
