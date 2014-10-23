dd3d4d57219297c2c96b6cecebb3cfba8a7aad7b
Niels Doucet <niels.doucet@mediafin.be>
2014-10-23 12:57:33 +0200

    Issue #47 move tests + remove duplicate libraries

--- across-core/src/test/java/com/foreach/across/test/cache/TestAcrossCompositeCacheManager.java
+++ across-core/src/test/java/com/foreach/across/test/cache/TestAcrossCompositeCacheManager.java
@@ -1,16 +1,21 @@
-package com.foreach.across.test;
+package com.foreach.across.test.cache;
 
 import com.foreach.across.config.AcrossContextConfigurer;
 import com.foreach.across.core.AcrossContext;
+import com.foreach.across.core.installers.InstallerAction;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -38,12 +43,23 @@ public class TestAcrossCompositeCacheManager
 	}
 
 	@Configuration
-	@AcrossTestConfiguration
-	static class Config implements AcrossContextConfigurer
+	static class Config
 	{
 
-		@Override
-		public void configure( AcrossContext context ) {
+		@Bean
+		public AcrossContext acrossContext( ConfigurableApplicationContext applicationContext ) {
+			Map<String, AcrossContextConfigurer> configurerMap =
+					applicationContext.getBeansOfType( AcrossContextConfigurer.class );
+
+			AcrossContext context = new AcrossContext( applicationContext );
+			context.setInstallerAction( InstallerAction.DISABLED );
+
+			for ( AcrossContextConfigurer configurer : configurerMap.values() ) {
+				configurer.configure( context );
+			}
+
+			return context;
 		}
+
 	}
 }
