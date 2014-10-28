19b3fe98dcd3777be021e33bd78825ebfe3d32ea
Arne Vandamme <arne@foreach.be>
2014-10-28 09:41:02 +0100

    Provide AcrossTestContext and AcrossTestWebContext for quickly bootstrapping contexts in unit tests without Spring runner

---
+++ across-test/src/main/java/com/foreach/across/test/AcrossTestContext.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.test;
+
+import com.foreach.across.config.AcrossContextConfigurer;
+import com.foreach.across.core.AcrossContext;
+import com.foreach.across.core.context.AcrossApplicationContext;
+import com.foreach.across.core.context.AcrossConfigurableApplicationContext;
+import com.foreach.across.core.context.AcrossContextUtils;
+import com.foreach.across.core.context.beans.ProvidedBeansMap;
+import com.foreach.across.core.context.beans.SingletonBean;
+import com.foreach.across.core.context.info.AcrossContextInfo;
+import com.foreach.across.core.context.registry.AcrossContextBeanRegistry;
+
+import java.io.Closeable;
+
+/**
+ * Creates and bootstraps a new AcrossContext instance using the configurers passed in
+ * as constructor parameters.  The default test datasource is used and no default modules
+ * are added to the context.
+ * <p/>
+ * The test context provides methods for easy querying of an AcrossContext and its modules.
+ * <p/>
+ * <strong>Note:</strong> when finished with an AcrossTestContext it is important that
+ * the {@link #close()} method is called.  To make this easier this class implements
+ * {@link java.io.Closeable}.
+ *
+ * @author Arne Vandamme
+ * @see com.foreach.across.test.AcrossTestWebContext
+ * @see com.foreach.across.test.AcrossTestConfiguration
+ */
+public class AcrossTestContext implements Closeable
+{
+	private final AcrossConfigurableApplicationContext applicationContext;
+	private final AcrossContext acrossContext;
+	private final AcrossContextBeanRegistry beanRegistry;
+	private final AcrossContextInfo contextInfo;
+
+	public AcrossTestContext( AcrossContextConfigurer... configurers ) {
+		applicationContext = createApplicationContext();
+
+		ProvidedBeansMap providedBeans = new ProvidedBeansMap();
+
+		for ( int i = 0; i < configurers.length; i++ ) {
+			providedBeans.put(
+					"QueryableAcrossTestContext.AcrossContextConfigurer~" + i,
+					new SingletonBean( configurers[i] )
+			);
+		}
+
+		applicationContext.provide( providedBeans );
+
+		applicationContext.refresh();
+		applicationContext.start();
+
+		acrossContext = applicationContext.getBean( AcrossContext.class );
+		beanRegistry = AcrossContextUtils.getBeanRegistry( acrossContext );
+		contextInfo = AcrossContextUtils.getContextInfo( acrossContext );
+	}
+
+	protected AcrossConfigurableApplicationContext createApplicationContext() {
+		AcrossApplicationContext ctx = new AcrossApplicationContext();
+		ctx.register( AcrossTestContextConfiguration.class );
+
+		return ctx;
+	}
+
+	@Override
+	public void close() {
+		acrossContext.shutdown();
+		applicationContext.stop();
+	}
+
+	public AcrossContextBeanRegistry beanRegistry() {
+		return beanRegistry;
+	}
+
+	public AcrossContextInfo contextInfo() {
+		return contextInfo;
+	}
+}
---
+++ across-test/src/test/java/com/foreach/across/it/ITAcrossTestContextBootstrap.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.it;
+
+import com.foreach.across.config.AcrossContextConfigurer;
+import com.foreach.across.core.AcrossContext;
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.Installer;
+import com.foreach.across.core.annotations.InstallerMethod;
+import com.foreach.across.core.context.configurer.ApplicationContextConfigurer;
+import com.foreach.across.core.installers.InstallerRunCondition;
+import com.foreach.across.modules.web.AcrossWebModule;
+import com.foreach.across.test.AcrossTestContext;
+import com.foreach.across.test.AcrossTestWebContext;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author Arne Vandamme
+ */
+public class ITAcrossTestContextBootstrap
+{
+	@Before
+	public void resetInstaller() {
+		SimpleInstaller.installed = false;
+	}
+
+	@Test
+	public void bootstrapWithoutWebModule() {
+		try (AcrossTestContext ctx = new AcrossTestContext( new Config() )) {
+			assertTrue( SimpleInstaller.installed );
+			assertTrue( ctx.contextInfo().getModuleInfo( "MyModule" ).isBootstrapped() );
+			assertFalse( ctx.contextInfo().hasModule( AcrossWebModule.NAME ) );
+		}
+	}
+
+	@Test
+	public void bootstrapWithWebModule() {
+		try (AcrossTestContext ctx = new AcrossTestWebContext( new Config() )) {
+			assertTrue( SimpleInstaller.installed );
+			assertTrue( ctx.contextInfo().getModuleInfo( "MyModule" ).isBootstrapped() );
+			assertTrue( ctx.contextInfo().hasModule( AcrossWebModule.NAME ) );
+		}
+	}
+
+	static class Config implements AcrossContextConfigurer
+	{
+		@Override
+		public void configure( AcrossContext context ) {
+			context.addModule( new MyModule() );
+		}
+	}
+
+	static class MyModule extends AcrossModule
+	{
+		@Override
+		public String getName() {
+			return "MyModule";
+		}
+
+		@Override
+		public String getDescription() {
+			return "Module that will force the installers to run.";
+		}
+
+		@Override
+		public Object[] getInstallers() {
+			return new Object[] { SimpleInstaller.class };
+		}
+
+		@Override
+		protected void registerDefaultApplicationContextConfigurers( Set<ApplicationContextConfigurer> contextConfigurers ) {
+		}
+	}
+
+	@Installer(
+			description = "Simple installer that will trigger the core installation.",
+			runCondition = InstallerRunCondition.VersionDifferent,
+			version = 1
+	)
+	static class SimpleInstaller
+	{
+		public static boolean installed = false;
+
+		@InstallerMethod
+		public void run() {
+			installed = true;
+		}
+	}
+}
