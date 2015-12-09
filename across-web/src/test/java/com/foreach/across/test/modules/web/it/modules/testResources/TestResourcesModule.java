ccefed26c66e3586885819d9bc685b3b48f0fe96
Arne Vandamme <arne@foreach.be>
2015-12-09 20:27:10 +0100

    AX-56 Add a AbstractWebIntegrationTest using Spring Boot and Embedded Tomcat (cherry picked from commit bb1c80e)

---
+++ across-web/src/test/java/com/foreach/across/test/modules/web/it/modules/testResources/TestResourcesModule.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.foreach.across.test.modules.web.it.modules.testResources;
+
+import com.foreach.across.core.AcrossModule;
+import com.foreach.across.core.annotations.AcrossDepends;
+import com.foreach.across.modules.web.AcrossWebModule;
+
+/**
+ * @author Arne Vandamme
+ */
+@AcrossDepends(required = AcrossWebModule.NAME)
+public class TestResourcesModule extends AcrossModule
+{
+	public static final String NAME = "TestResourcesModule";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public String getResourcesKey() {
+		return "testResources";
+	}
+
+	@Override
+	public String getDescription() {
+		return "Test module that contains some static resources.";
+	}
+}
