eccce41ddfd1e23d464b31291c0932eaf771b144
Marc Vanbrabant <marc@foreach.be>
2015-12-28 11:24:23 +0100

    AX-102 Don't create an inner bean

--- across-core/src/main/java/com/foreach/across/core/installers/AcrossCoreSchemaInstaller.java
+++ across-core/src/main/java/com/foreach/across/core/installers/AcrossCoreSchemaInstaller.java
@@ -16,10 +16,11 @@
 
 package com.foreach.across.core.installers;
 
+import liquibase.exception.LiquibaseException;
 import liquibase.integration.spring.SpringLiquibase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
@@ -32,22 +33,21 @@ public class AcrossCoreSchemaInstaller
 	private static final Logger LOG = LoggerFactory.getLogger( AcrossCoreSchemaInstaller.class );
 
 	private final DataSource dataSource;
-	private final AutowireCapableBeanFactory beanFactory;
+	private final ApplicationContext applicationContext;
 
-	public AcrossCoreSchemaInstaller( DataSource dataSource, AutowireCapableBeanFactory beanFactory ) {
+	public AcrossCoreSchemaInstaller( DataSource dataSource, ApplicationContext applicationContext ) {
 		this.dataSource = dataSource;
-		this.beanFactory = beanFactory;
+		this.applicationContext = applicationContext;
 	}
 
 	@PostConstruct
-	protected void installCoreSchema() {
+	protected void installCoreSchema() throws LiquibaseException {
 		LOG.info( "Installing the core schema for Across" );
 
 		SpringLiquibase liquibase = new SpringLiquibase();
 		liquibase.setChangeLog( "classpath:" + getClass().getName().replace( '.', '/' ) + ".xml" );
 		liquibase.setDataSource( dataSource );
-
-		beanFactory.autowireBeanProperties( liquibase, AutowireCapableBeanFactory.AUTOWIRE_NO, false );
-		beanFactory.initializeBean( liquibase, "(inner bean)#springLiquibase" );
+		liquibase.setResourceLoader( applicationContext );
+		liquibase.afterPropertiesSet();
 	}
 }
