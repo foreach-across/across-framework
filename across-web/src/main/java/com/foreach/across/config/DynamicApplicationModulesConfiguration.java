239817a2b0410df413eca61fc4e64324a2d0f65d
Arne Vandamme <arne@foreach.be>
2016-02-15 20:59:44 +0100

    AX-40 dynamic application modules configuration (wip)

--- across-web/src/main/java/com/foreach/across/config/DynamicApplicationModulesConfiguration.java
+++ across-web/src/main/java/com/foreach/across/config/DynamicApplicationModulesConfiguration.java
@@ -17,16 +17,24 @@ package com.foreach.across.config;
 
 import com.foreach.across.core.AcrossContext;
 import com.foreach.across.core.AcrossException;
+import com.foreach.across.core.AcrossModule;
 import com.foreach.across.core.DynamicAcrossModuleFactory;
 import com.foreach.across.core.context.AcrossModuleRole;
 import com.foreach.across.core.context.ClassPathScanningChildPackageProvider;
+import com.foreach.across.core.context.ModuleDependencyResolver;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportAware;
 import org.springframework.core.type.AnnotationMetadata;
+
+import java.util.Optional;
 
 /**
  * When imported on a configuration class, this implementation will attempt to dynamically add generated modules.
@@ -37,15 +45,22 @@ import org.springframework.core.type.AnnotationMetadata;
  * @author Arne Vandamme
  */
 @Configuration
-public class DynamicApplicationModulesConfiguration implements AcrossContextConfigurer, ImportAware
+public class DynamicApplicationModulesConfiguration implements AcrossContextConfigurer, BeanFactoryAware, ImportAware
 {
 	private static final Logger LOG = LoggerFactory.getLogger( DynamicApplicationModulesConfiguration.class );
 
 	private AnnotationMetadata importMetadata;
 
+	private BeanFactory beanFactory;
+
 	@Override
 	public void setImportMetadata( AnnotationMetadata importMetadata ) {
 		this.importMetadata = importMetadata;
+	}
+
+	@Override
+	public void setBeanFactory( BeanFactory beanFactory ) throws BeansException {
+		this.beanFactory = beanFactory;
 	}
 
 	@Override
@@ -71,78 +86,85 @@ public class DynamicApplicationModulesConfiguration implements AcrossContextConf
 
 	private void configureApplicationModule( AcrossContext context, String moduleBasePackage, String baseModuleName ) {
 		String applicationModuleName = baseModuleName + "ApplicationModule";
+		String resourcesKey = StringUtils.uncapitalize( baseModuleName );
 
 		if ( context.getModule( applicationModuleName ) == null ) {
-			String resourcesKey = StringUtils.uncapitalize( baseModuleName );
+			Optional<AcrossModule> module = resolveModule( applicationModuleName );
 
-			LOG.info( "Adding package based application module {}, resources: {}, base package: {}",
-			          applicationModuleName, resourcesKey, moduleBasePackage );
-
-			DynamicAcrossModuleFactory factory = new DynamicAcrossModuleFactory()
-					.setModuleRole( AcrossModuleRole.APPLICATION )
-					.setModuleName( applicationModuleName )
-					.setResourcesKey( resourcesKey )
-					.setBasePackage( moduleBasePackage );
-
-			try {
-				context.addModule( factory.getObject() );
-			}
-			catch ( Exception e ) {
-				LOG.error( "Unable to add package based application module", e );
-			}
+			context.addModule( module.orElseGet( () -> dynamicModule(
+					AcrossModuleRole.APPLICATION,
+					applicationModuleName,
+					resourcesKey,
+					moduleBasePackage
+			) ) );
 		}
 	}
 
 	private void configureInfrastructureModule( AcrossContext context,
 	                                            String moduleBasePackage,
 	                                            String baseModuleName ) {
-		String applicationModuleName = baseModuleName + "InfrastructureModule";
+		String infrastructureModule = baseModuleName + "InfrastructureModule";
+		String resourcesKey = StringUtils.uncapitalize( baseModuleName + "Infrastructure" );
 
-		if ( context.getModule( applicationModuleName ) == null ) {
-			String resourcesKey = StringUtils.uncapitalize( baseModuleName + "Infrastructure" );
+		if ( context.getModule( infrastructureModule ) == null ) {
+			Optional<AcrossModule> module = resolveModule( infrastructureModule );
 
-			LOG.info( "Adding package based infrastructure module {}, resources: {}, base package: {}",
-			          applicationModuleName, resourcesKey, moduleBasePackage );
-
-			DynamicAcrossModuleFactory factory = new DynamicAcrossModuleFactory()
-					.setModuleRole( AcrossModuleRole.INFRASTRUCTURE )
-					.setModuleName( applicationModuleName )
-					.setResourcesKey( resourcesKey )
-					.setBasePackage( moduleBasePackage );
-
-			try {
-				context.addModule( factory.getObject() );
-			}
-			catch ( Exception e ) {
-				LOG.error( "Unable to add package based infrastructure module", e );
-			}
+			context.addModule( module.orElseGet( () -> dynamicModule(
+					AcrossModuleRole.INFRASTRUCTURE,
+					infrastructureModule,
+					resourcesKey,
+					moduleBasePackage
+			) ) );
 		}
 	}
 
 	private void configurePostProcessorModule( AcrossContext context,
 	                                           String moduleBasePackage,
 	                                           String baseModuleName ) {
-		String applicationModuleName = baseModuleName + "PostProcessorModule";
+		String postprocessorModule = baseModuleName + "PostProcessorModule";
+		String resourcesKey = StringUtils.uncapitalize( baseModuleName + "PostProcessor" );
 
-		if ( context.getModule( applicationModuleName ) == null ) {
-			String resourcesKey = StringUtils.uncapitalize( baseModuleName + "PostProcessor" );
+		if ( context.getModule( postprocessorModule ) == null ) {
+			Optional<AcrossModule> module = resolveModule( postprocessorModule );
 
-			LOG.info( "Adding package based postprocessor module {}, resources: {}, base package: {}",
-			          applicationModuleName, resourcesKey, moduleBasePackage );
-
-			DynamicAcrossModuleFactory factory = new DynamicAcrossModuleFactory()
-					.setModuleRole( AcrossModuleRole.POSTPROCESSOR )
-					.setModuleName( applicationModuleName )
-					.setResourcesKey( resourcesKey )
-					.setBasePackage( moduleBasePackage );
-
-			try {
-				context.addModule( factory.getObject() );
-			}
-			catch ( Exception e ) {
-				LOG.error( "Unable to add package based postprocessor module", e );
-			}
+			context.addModule( module.orElseGet( () -> dynamicModule(
+					AcrossModuleRole.POSTPROCESSOR,
+					postprocessorModule,
+					resourcesKey,
+					moduleBasePackage
+			) ) );
 		}
+	}
+
+	private AcrossModule dynamicModule( AcrossModuleRole moduleRole,
+	                                    String moduleName,
+	                                    String resourcesKey,
+	                                    String moduleBasePackage ) {
+		LOG.info( "Adding package based {} module {}, resources: {}, base package: {}",
+		          moduleRole.name(), moduleName, resourcesKey, moduleBasePackage );
+
+		DynamicAcrossModuleFactory factory = new DynamicAcrossModuleFactory()
+				.setModuleRole( moduleRole )
+				.setModuleName( moduleName )
+				.setResourcesKey( resourcesKey )
+				.setBasePackage( moduleBasePackage );
+
+		try {
+			return factory.getObject();
+		}
+		catch ( Exception e ) {
+			throw new AcrossException( "Unable to create package based module", e );
+		}
+	}
+
+	private Optional<AcrossModule> resolveModule( String moduleName ) {
+		try {
+			return beanFactory.getBean( ModuleDependencyResolver.class ).resolveModule( moduleName, true );
+		}
+		catch ( NoSuchBeanDefinitionException ignore ) {
+		}
+
+		return Optional.empty();
 	}
 
 	private Class<?> importingClass() {
