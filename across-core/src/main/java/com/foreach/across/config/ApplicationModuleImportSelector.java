9aadef37bc723c80251a38965edf2771f217b1e1
Arne Vandamme <arne@foreach.be>
2019-01-02 10:50:46 +0100

    Remove compiler warning due to missing jsr305 dependency (caused by @Nullable api).

--- across-core/src/main/java/com/foreach/across/config/ApplicationModuleImportSelector.java
+++ across-core/src/main/java/com/foreach/across/config/ApplicationModuleImportSelector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors
+ * Copyright 2019 the original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +39,7 @@ public class ApplicationModuleImportSelector implements ImportSelector, Environm
 {
 	private ConfigurableEnvironment environment;
 
+	@SuppressWarnings("unchecked")
 	private static Map<String, Object> getOrAdd( MutablePropertySources sources, String name ) {
 		if ( sources.contains( name ) ) {
 			return (Map<String, Object>) sources.get( name ).getSource();
