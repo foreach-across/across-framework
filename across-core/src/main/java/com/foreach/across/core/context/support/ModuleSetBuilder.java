b185b519c1789e06ca1902bf3dd2e8cdd84ec3cd
Arne Vandamme <arne@foreach.be>
2019-01-15 09:24:26 +0100

    Add new AcrossModuleDescriptorSetBuilder which resolves configured modules to the entire set of AcrossModuleDescriptors that should be loaded

--- across-core/src/main/java/com/foreach/across/core/context/support/ModuleSetBuilder.java
+++ across-core/src/main/java/com/foreach/across/core/context/support/ModuleSetBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 the original author or authors
+ * Copyright 2019 the original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,9 @@ import java.util.function.Supplier;
  * Helper that builds a {@link ModuleSet}.
  *
  * @author Arne Vandamme
+ * @deprecated see {@link com.foreach.across.core.context.module.AcrossModuleDescriptorSetBuilder}
  */
+@Deprecated
 public class ModuleSetBuilder
 {
 	private static final Logger LOG = LoggerFactory.getLogger( ModuleSetBuilder.class );
