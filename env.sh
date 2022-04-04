5a797f5d901094be0af96d57f1d44fdd02325120
Davy Verstappen <dvsp@foreach.be>
2022-04-04 08:02:50 +0200

    Document maven properties, especially how and why they are duplicated.

--- env.sh
+++ env.sh
@@ -3,8 +3,8 @@ echo "Warning: Across is still using old plugins that are not multi-thread safe"
 alias mi='mvn install'
 alias mci='mvn clean install'
 # TODO: add -T1C when multi-thread issue is fixed:
-alias miwt='mvn install -DskipTests'
-alias mciwt='mvn clean install -DskipTests'
+alias miwt='mvn install -DskipTests -Dmaven.javadoc.skip'
+alias mciwt='mvn clean install -DskipTests -Dmaven.javadoc.skip'
 
 export JAVA8_HOME=~/.jdks/temurin-1.8.0_322
 export JAVA_HOME=${JAVA8_HOME}
