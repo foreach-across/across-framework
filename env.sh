bfaeb4e004d87e04a37c769b8afec8218da38862
Davy Verstappen <davy.verstappen@iodigital.com>
2023-05-12 00:31:34 +0200

    Remove maven warnings about unmanaged plugin versions:
    This will also remove those warnings in all modules/applications.

--- env.sh
+++ env.sh
@@ -1,3 +1,11 @@
+export JAVA8_HOME=~/.jdks/1.8
+export JAVA_HOME=${JAVA8_HOME}
+export PATH=$JAVA_HOME/bin:$PATH
+
+# Keep this in sync with across-bamboo-specs/docker-compose-ax-5.yml
+export MAVEN_HOME=~/tools/apache-maven-3.5.2
+export PATH=$MAVEN_HOME/bin:$PATH
+
 echo "Warning: Across is still using old plugins that are not multi-thread safe"
 
 alias mi='mvn install'
@@ -5,7 +13,3 @@ alias mci='mvn clean install'
 # TODO: add -T1C when multi-thread issue is fixed:
 alias miwt='mvn install -DskipTests -Dmaven.javadoc.skip=true -Djacoco.skip=true'
 alias mciwt='mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Djacoco.skip=true'
-
-export JAVA8_HOME=~/.jdks/1.8
-export JAVA_HOME=${JAVA8_HOME}
-export PATH=$JAVA_HOME/bin:$PATH
