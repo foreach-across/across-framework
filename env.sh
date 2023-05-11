b6ff34af4b1827c3bc5e7f8f4bc85301e83be82d
Davy Verstappen <davy.verstappen@iodigital.com>
2023-05-11 23:53:25 +0200

    Change revision to 5.2.1-SNAPSHOT, so we do not impact the Bis projects:
    These have been released with 5.2.0-SNAPSHOT.

--- env.sh
+++ env.sh
@@ -3,8 +3,8 @@ echo "Warning: Across is still using old plugins that are not multi-thread safe"
 alias mi='mvn install'
 alias mci='mvn clean install'
 # TODO: add -T1C when multi-thread issue is fixed:
-alias miwt='mvn install -DskipTests'
-alias mciwt='mvn clean install -DskipTests'
+alias miwt='mvn install -DskipTests -Dmaven.javadoc.skip=true -Djacoco.skip=true'
+alias mciwt='mvn clean install -DskipTests -Dmaven.javadoc.skip=true -Djacoco.skip=true'
 
 export JAVA8_HOME=~/.jdks/1.8
 export JAVA_HOME=${JAVA8_HOME}
